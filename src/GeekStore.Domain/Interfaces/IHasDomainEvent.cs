b0ff4d6fe0106aced9622ad9f50fc874c67a99a5
Caio Vinicius De Lima <32105582+caiovlima@users.noreply.github.com>
2021-08-05 21:53:32 -0300

    Implement Domain Event

---
+++ src/GeekStore.Domain/Interfaces/IHasDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using GeekStore.Domain.Events;
+
+namespace GeekStore.Domain.Interfaces
+{
+    public interface IHasDomainEvent
+    {
+        public List<DomainEvent> DomainEvents { get; set; }
+    }
+}
