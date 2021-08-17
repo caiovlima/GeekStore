3cd8200ed1c513ac9802f7b6ae09626bf113c2af
Caio Vinicius De Lima <32105582+caiovlima@users.noreply.github.com>
2021-08-16 21:20:03 -0300

    adicionando camada de webapi

--- src/GeekStore.Infra/Setup/DependencyInjection.cs
+++ src/GeekStore.Infra/Setup/DependencyInjection.cs
@@ -1,15 +1,19 @@
 ﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using GeekStore.Application.Product.Interfaces;
+using GeekStore.Infra.Repository;
 
 
 namespace GeekStore.Infra.Setup
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfra(this IServiceCollection services)
+        public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration configuration)
         {
             //services.AddAutoMapper(Assembly.GetExecutingAssembly());
             //services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddScoped<IProductRepository, ProductRepository>();
             return services;
         }
     }
